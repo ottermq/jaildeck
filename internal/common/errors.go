4c0af04d91acb8ff095b9d166e0f7a77982fbbfe
Andre Luis da Cunha <andreluiscunha81@gmail.com>
2026-08-16 16:55:25 -0300

    chore: remove unused AppError sentinels
    Owner/User/Workspace/Project IDs aren't concepts jaildeck has.

--- internal/common/errors.go
+++ internal/common/errors.go
@@ -8,14 +8,10 @@ import (
 )
 
 var (
-	ErrInvalidOwnerID     = NewAppError(http.StatusBadRequest, "invalid owner ID")
-	ErrInvalidUserID      = NewAppError(http.StatusBadRequest, "invalid user ID")
-	ErrInvalidWorkspaceID = NewAppError(http.StatusBadRequest, "invalid workspace ID")
-	ErrInvalidProjectID   = NewAppError(http.StatusBadRequest, "invalid project ID")
-	ErrForbidden          = NewAppError(http.StatusForbidden, "forbidden")
-	ErrUnauthorized       = NewAppError(http.StatusUnauthorized, "unauthorized")
-	ErrBadRequest         = NewAppError(http.StatusBadRequest, "invalid request")
-	ErrInvalidAssigneeID  = NewAppError(http.StatusBadRequest, "invalid assignee ID")
+	ErrForbidden         = NewAppError(http.StatusForbidden, "forbidden")
+	ErrUnauthorized      = NewAppError(http.StatusUnauthorized, "unauthorized")
+	ErrBadRequest        = NewAppError(http.StatusBadRequest, "invalid request")
+	ErrInvalidAssigneeID = NewAppError(http.StatusBadRequest, "invalid assignee ID")
 )
 
 type AppError struct {
